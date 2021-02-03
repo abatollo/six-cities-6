9afd294f9749fad19ba54702a5b49d63b327240b
Aleksandr Batollo <a@batollo.ru>
2021-02-03 13:32:47 +0300

    Создал компонент главной страницы и передал в него пропсы

---
+++ src/components/app/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MainPage from "../main-page/main-page";
+
+
+const App = (props) => {
+  const {placesCount} = props;
+
+  return (
+    <MainPage placesCount={placesCount}/>
+  );
+};
+
+App.propTypes = {
+  placesCount: PropTypes.number.isRequired,
+};
+
+export default App;
