463a2101e520c6c408b00a5cd2a657169fe33ebb
Aleksandr Batollo <a@batollo.ru>
2021-02-08 17:46:00 +0300

    Валидирует все типы данных на главном экране и переносит моки в отдельный файл

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -107,7 +107,42 @@ const MainPage = (props) => {
 MainPage.propTypes = {
   hotels: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number.isRequired
+        "bedrooms": PropTypes.number.isRequired,
+        "city": PropTypes.shape({
+          location: PropTypes.shape({
+            latitude: PropTypes.number.isRequired,
+            longitude: PropTypes.number.isRequired,
+            zoom: PropTypes.number.isRequired
+          }),
+          name: PropTypes.string.isRequired
+        }),
+        "description": PropTypes.string.isRequired,
+        "goods": PropTypes.arrayOf(
+            PropTypes.string.isRequired
+        ),
+        "host": PropTypes.shape({
+          "avatar_url": PropTypes.string.isRequired,
+          "id": PropTypes.number.isRequired,
+          "is_pro": PropTypes.bool.isRequired,
+          "name": PropTypes.string.isRequired
+        }),
+        "id": PropTypes.number.isRequired,
+        "images": PropTypes.arrayOf(
+            PropTypes.string.isRequired
+        ),
+        "is_favorite": PropTypes.bool.isRequired,
+        "is_premium": PropTypes.bool.isRequired,
+        "location": PropTypes.shape({
+          "latitude": PropTypes.number.isRequired,
+          "longitude": PropTypes.number.isRequired,
+          "zoom": PropTypes.number.isRequired
+        }),
+        "max_adults": PropTypes.number.isRequired,
+        "preview_image": PropTypes.string.isRequired,
+        "price": PropTypes.number.isRequired,
+        "rating": PropTypes.number.isRequired,
+        "title": PropTypes.string.isRequired,
+        "type": PropTypes.string.isRequired
       })).isRequired
 };
 
