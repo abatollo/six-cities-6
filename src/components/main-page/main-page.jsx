de9b3f7922e7a35b4fcad764488d8ef1fa5cf423
Aleksandr Batollo <a@batollo.ru>
2021-02-08 18:02:19 +0300

    Меняет ключи объектов в моковых данных и при валидации со snake_case на camelCase

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -107,8 +107,8 @@ const MainPage = (props) => {
 MainPage.propTypes = {
   hotels: PropTypes.arrayOf(
       PropTypes.shape({
-        "bedrooms": PropTypes.number.isRequired,
-        "city": PropTypes.shape({
+        bedrooms: PropTypes.number.isRequired,
+        city: PropTypes.shape({
           location: PropTypes.shape({
             latitude: PropTypes.number.isRequired,
             longitude: PropTypes.number.isRequired,
@@ -116,33 +116,33 @@ MainPage.propTypes = {
           }),
           name: PropTypes.string.isRequired
         }),
-        "description": PropTypes.string.isRequired,
-        "goods": PropTypes.arrayOf(
+        description: PropTypes.string.isRequired,
+        goods: PropTypes.arrayOf(
             PropTypes.string.isRequired
         ),
-        "host": PropTypes.shape({
-          "avatar_url": PropTypes.string.isRequired,
-          "id": PropTypes.number.isRequired,
-          "is_pro": PropTypes.bool.isRequired,
-          "name": PropTypes.string.isRequired
+        host: PropTypes.shape({
+          avatarUrl: PropTypes.string.isRequired,
+          id: PropTypes.number.isRequired,
+          isPro: PropTypes.bool.isRequired,
+          name: PropTypes.string.isRequired
         }),
-        "id": PropTypes.number.isRequired,
-        "images": PropTypes.arrayOf(
+        id: PropTypes.number.isRequired,
+        images: PropTypes.arrayOf(
             PropTypes.string.isRequired
         ),
-        "is_favorite": PropTypes.bool.isRequired,
-        "is_premium": PropTypes.bool.isRequired,
-        "location": PropTypes.shape({
-          "latitude": PropTypes.number.isRequired,
-          "longitude": PropTypes.number.isRequired,
-          "zoom": PropTypes.number.isRequired
+        isFavorite: PropTypes.bool.isRequired,
+        isPremium: PropTypes.bool.isRequired,
+        location: PropTypes.shape({
+          latitude: PropTypes.number.isRequired,
+          longitude: PropTypes.number.isRequired,
+          zoom: PropTypes.number.isRequired
         }),
-        "max_adults": PropTypes.number.isRequired,
-        "preview_image": PropTypes.string.isRequired,
-        "price": PropTypes.number.isRequired,
-        "rating": PropTypes.number.isRequired,
-        "title": PropTypes.string.isRequired,
-        "type": PropTypes.string.isRequired
+        maxAdults: PropTypes.number.isRequired,
+        previewImage: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired
       })).isRequired
 };
 
