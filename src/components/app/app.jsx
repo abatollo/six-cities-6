3c3dca878324eb11516a122b9467a15d37278c6d
Aleksandr Batollo <a@batollo.ru>
2021-02-09 17:12:22 +0300

    Вынесет настройки путей и шапку в отдельные файлы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
+import {Routes} from "../../config";
 import MainScreen from "../main-screen/main-screen";
 import LoginScreen from "../login-screen/login-screen";
 import FavoritesScreen from "../favorites-screen/favorites-screen";
 import PropertyScreen from "../property-screen/property-screen";
 import NotFoundScreen from "../not-found-screen/not-found-screen";
-
-const Routes = {
-  MAIN: `/`,
-  LOGIN: `/login`,
-  FAVORITES: `/favorites`,
-  HOTELS: `/hotels/:id`
-};
 
 const App = (props) => {
   const {hotels} = props;
