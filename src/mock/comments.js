9e9c33d04741c1b8d3be88ac940c61584fe7717c
Aleksandr Batollo <a@batollo.ru>
2021-02-16 13:59:22 +0300

    Добавит моки комментариев

---
+++ src/mock/comments.js
@@ -0,0 +1,54 @@
+const COMMENTS = [
+  {
+    comment: `1 A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: `img/1.png`,
+      id: 4,
+      isPro: false,
+      name: `Max`
+    }
+  },
+  {
+    comment: `2 A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 2,
+    rating: 4,
+    user: {
+      avatarUrl: `img/1.png`,
+      id: 4,
+      isPro: false,
+      name: `Max`
+    }
+  },
+  {
+    comment: `3 A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 3,
+    rating: 4,
+    user: {
+      avatarUrl: `img/1.png`,
+      id: 4,
+      isPro: false,
+      name: `Max`
+    }
+  },
+  {
+    comment: `4 A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`,
+    id: 4,
+    rating: 4,
+    user: {
+      avatarUrl: `img/1.png`,
+      id: 4,
+      isPro: false,
+      name: `Max`
+    }
+  }
+];
+
+export const getComments = () => {
+  return COMMENTS;
+};
