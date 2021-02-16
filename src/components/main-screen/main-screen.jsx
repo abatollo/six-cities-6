f29ad952030d5f9f75401fb7faf497b4ba33715e
Aleksandr Batollo <a@batollo.ru>
2021-02-16 17:27:57 +0300

    Вынесет объект для валидации пропсов в utils

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Header from "../header/header";
 import PlaceCard from "../place-card/place-card";
+import {PropsValidator} from "../../utils";
 
 const MainScreen = (props) => {
   const {hotels} = props;
@@ -85,45 +86,7 @@ const MainScreen = (props) => {
 };
 
 MainScreen.propTypes = {
-  hotels: PropTypes.arrayOf(
-      PropTypes.shape({
-        bedrooms: PropTypes.number.isRequired,
-        city: PropTypes.shape({
-          location: PropTypes.shape({
-            latitude: PropTypes.number.isRequired,
-            longitude: PropTypes.number.isRequired,
-            zoom: PropTypes.number.isRequired
-          }),
-          name: PropTypes.string.isRequired
-        }),
-        description: PropTypes.string.isRequired,
-        goods: PropTypes.arrayOf(
-            PropTypes.string.isRequired
-        ),
-        host: PropTypes.shape({
-          avatarUrl: PropTypes.string.isRequired,
-          id: PropTypes.number.isRequired,
-          isPro: PropTypes.bool.isRequired,
-          name: PropTypes.string.isRequired
-        }),
-        id: PropTypes.number.isRequired,
-        images: PropTypes.arrayOf(
-            PropTypes.string.isRequired
-        ),
-        isFavorite: PropTypes.bool.isRequired,
-        isPremium: PropTypes.bool.isRequired,
-        location: PropTypes.shape({
-          latitude: PropTypes.number.isRequired,
-          longitude: PropTypes.number.isRequired,
-          zoom: PropTypes.number.isRequired
-        }),
-        maxAdults: PropTypes.number.isRequired,
-        previewImage: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-        rating: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired
-      })).isRequired
+  hotels: PropsValidator.HOTELS
 };
 
 export default MainScreen;
