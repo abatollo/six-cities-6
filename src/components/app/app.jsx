c6ef9eae08c7cca1f468c1f1384ff5b1b3013bf7
Aleksandr Batollo <a@batollo.ru>
2021-03-27 18:51:18 +0300

    Добавит загрузку данных с сервера

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {connect} from "react-redux";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import {Routes} from "../../routes";
+import PropTypes from "prop-types";
 import MainScreen from "../main-screen/main-screen";
 import LoginScreen from "../login-screen/login-screen";
 import FavoritesScreen from "../favorites-screen/favorites-screen";
 import PropertyScreen from "../property-screen/property-screen";
 import NotFoundScreen from "../not-found-screen/not-found-screen";
+import LoadingScreen from "../loading-screen/loading-screen";
+import {fetchHotelsList} from "../../store/api-actions";
 
-const App = () => {
+const App = ({isDataLoaded, onLoadData}) => {
+
+  useEffect(() => {
+    if (!isDataLoaded) {
+      onLoadData();
+    }
+  }, [isDataLoaded]);
+
+  if (!isDataLoaded) {
+    return (
+      <LoadingScreen />
+    );
+  }
+
   return (
     <BrowserRouter>
       <Switch>
@@ -31,4 +48,19 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+  isDataLoaded: state.cities.isDataLoaded,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onLoadData() {
+    dispatch(fetchHotelsList());
+  },
+});
+
+App.propTypes = {
+  isDataLoaded: PropTypes.bool.isRequired,
+  onLoadData: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import cities from './cities/reducer';
 import sort from './sort/reducer';
+import authorization from './authorization/reducer';
 
 export const rootReducer = combineReducers({
   cities,
-  sort
+  sort,
+  authorization
 });
