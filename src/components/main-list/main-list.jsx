dfabc645f223d1ef71efd822fd9b3427c93ba5a6
Aleksandr Batollo <a@batollo.ru>
2021-04-05 19:56:31 +0300

    Исправит разметку пустого экрана на главной странице

--- src/components/main-list/main-list.jsx
+++ src/components/main-list/main-list.jsx
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import MainCard from '../main-card/main-card';
+import Map from '../map/map';
+import MainMap from '../map-main/map-main';
 
 import {PropsValidator} from '../../utils/props-validator';
 import MainListSorting from '../main-list-sorting/main-list-sorting';
@@ -11,19 +13,33 @@ import {ActionCreator} from '../../store/action';
 
 const MainList = ({sortedFilteredHotels, currentCity, onMouseOver}) => {
   return (
-    <section className="cities__places places">
-      <h2 className="visually-hidden">Places</h2>
-      <b className="places__found">{sortedFilteredHotels.length} places to stay in {currentCity}</b>
-      <MainListSorting />
-      <div className="cities__places-list places__list tabs__content">
-        {sortedFilteredHotels.map((hotel) =>
-          <MainCard
-            key={hotel.id}
-            hotel={hotel}
-            onMouseOver={() => onMouseOver(hotel.id)}
-          />)}
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">{sortedFilteredHotels.length} places to stay in {currentCity}</b>
+        <MainListSorting />
+        <div className="cities__places-list places__list tabs__content">
+          {sortedFilteredHotels.map((hotel) =>
+            <MainCard
+              key={hotel.id}
+              hotel={hotel}
+              onMouseOver={() => onMouseOver(hotel.id)}
+            />)}
+        </div>
+      </section>
+      <div className="cities__right-section">
+        {
+          sortedFilteredHotels.length ?
+            <Map
+              points={sortedFilteredHotels}
+              render={(ref) => {
+                return (<MainMap mapRef={ref} />);
+              }}
+            /> :
+            ``
+        }
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -4,10 +4,8 @@ import PropTypes from 'prop-types';
 
 import Header from '../header/header';
 import MainList from '../main-list/main-list';
-import Map from '../map/map';
 import CitiesList from '../cities-list/cities-list';
 import MainListEmpty from '../main-list-empty/main-list-empty';
-import MainMap from '../map-main/map-main';
 import LoadingScreen from '../loading-screen/loading-screen';
 
 import {filterHotelsByCity} from '../../utils/filter-hotels-by-city';
@@ -20,7 +18,7 @@ const MainScreen = ({sortedFilteredHotels, isHotelsLoading, onLoadData}) => {
     onLoadData();
   }, []);
 
-  if (isHotelsLoading || !sortedFilteredHotels.length) {
+  if (isHotelsLoading) {
     return (
       <LoadingScreen />
     );
@@ -29,27 +27,17 @@ const MainScreen = ({sortedFilteredHotels, isHotelsLoading, onLoadData}) => {
   return (
     <div className="page page--gray page--main">
       <Header />
-      <main className="page__main page__main--index">
+      <main className={`${!sortedFilteredHotels.length ? `page__main--index-empty` : ``} page__main page__main--index`}>
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <CitiesList />
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            {
-              sortedFilteredHotels.length ?
-                <MainList sortedFilteredHotels={sortedFilteredHotels} /> :
-                <MainListEmpty />
-            }
-            <div className="cities__right-section">
-              <Map
-                points={sortedFilteredHotels}
-                render={(ref) => {
-                  return (<MainMap mapRef={ref} />);
-                }}
-              />
-            </div>
-          </div>
+          {
+            sortedFilteredHotels.length ?
+              <MainList sortedFilteredHotels={sortedFilteredHotels} /> :
+              <MainListEmpty />
+          }
         </div>
       </main>
     </div>
