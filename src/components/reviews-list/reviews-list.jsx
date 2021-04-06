2b8e7c7a62f2ce082284114826ff22cd3b2f8cdb
Aleksandr Batollo <a@batollo.ru>
2021-04-06 04:03:03 +0300

    Добавит сортировку комментариев

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 import ReviewsItem from '../reviews-item/reviews-item';
 
 import {PropsValidator} from '../../utils/props-validator';
+import {sortComments} from '../../utils/sort-comments';
 
 const ReviewsList = ({comments}) => {
+  const sortedComments = sortComments(comments);
   return (
     <>
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{comments.length}</span></h2>
       <ul className="reviews__list">
-        {comments.map((comment) =>
+        {sortedComments.map((comment) =>
           <ReviewsItem
             key={comment.id}
             comment={comment}
